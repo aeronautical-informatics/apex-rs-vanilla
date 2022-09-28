988e3e3270bd0be47d3771a114256492f8c27ff8
Wanja Zaeske <wanja.zaeske@dlr.de>
2022-09-28 17:10:15 +0200

    small QOL improvements + fix wrong type casts for sampling and queuing port + get rid of warnings for generated code

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,15 @@
 #![deny(rustdoc::broken_intra_doc_links)]
 
 /// This module contains bindings to the services defined by ARINC653
-pub mod bindings;
+pub mod bindings {
+    #![allow(clippy::redundant_static_lifetimes)]
+    #![allow(dead_code)]
+    #![allow(missing_docs)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
+    #![allow(non_upper_case_globals)]
+    include!("bindings.rs");
+}
 
 /// This module contains the mappings from [crate::bindings] to [apex_rs::bindings]
 pub mod apex;
